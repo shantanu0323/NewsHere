9ddb2d26e3b1ea4c5b63ecedbdc7704ac3f0caae
shantanu0323 <srs.shaan@gmail.com>
2018-01-27 23:42:30 +0530

    intermediate: url generation for categories

--- app/src/main/java/com/shaan/newshere/CategoryActivity.java
+++ app/src/main/java/com/shaan/newshere/CategoryActivity.java
@@ -1,8 +1,14 @@
 package com.shaan.newshere;
 
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -11,71 +17,158 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.AdapterView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class CategoryActivity extends AppCompatActivity {
+public class CategoryActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG = "CategoryActivity";
 
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
-    private ViewPager mViewPager;
+    private ViewPager rootViewPager;
+
+    String[] countryNames = {"India", "Australia", "Canada", "China", "France", "Germany", "Italy", "Japan"};
+    int flags[] = {R.drawable.india_in, R.drawable.australia_au, R.drawable.canada_ca, R.drawable.china_cn, R.drawable.france_fr,
+            R.drawable.germany_de, R.drawable.italy_it, R.drawable.japan_jp};
+
+    private static Spinner spinner;
+
+    private static Typeface ROBOTO_THIN = null;
+    private static Typeface ROBOTO_LIGHT = null;
+
+    private ProgressBar loadingIndicator;
+    private static TabLayout tabLayout;
+
+    private static String BASE_URL = "https://newsapi.org/v2/top-headlines?apiKey=d98a0731ab834d3cb605d8ac24dd7072";
+    public static String[] countryCodes = {"in", "au", "ca", "cn", "fr", "de", "it", "jp"};
+    public static String[] categories = {"Business", "Entertainment", "Politics", "General", "Health", "Science", "Sports", "Technology"};
+
+    private static final String POSITION = "position";
+    private static final String TAB_POSITION = "spinner_pos";
+
+    private static View fragmentView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
 
+        findViews();
+        initFonts();
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                getSupportFragmentManager().beginTransaction().replace(fragmentView.getId(),
+                        PlaceholderFragment.newInstance(position, spinner.getSelectedItemPosition())).commit();
+
+//                previousCountryCode = COUNTRY_CODE;
+//                COUNTRY_CODE = countryCodes[position];
+//                if (TOP_HEADLINES_URL.endsWith("=")) {
+//                    TOP_HEADLINES_URL += COUNTRY_CODE;
+//                } else {
+//                    TOP_HEADLINES_URL = TOP_HEADLINES_URL.substring(0,TOP_HEADLINES_URL.length()-2) + COUNTRY_CODE;
+//                }
+//                Log.i(TAG, "onItemSelected: CALLED : " + TOP_HEADLINES_URL);
+//                initiateLoader();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+//                previousCountryCode = COUNTRY_CODE;
+//                COUNTRY_CODE = "in";
+//                if (TOP_HEADLINES_URL.endsWith("=")) {
+//                    TOP_HEADLINES_URL += COUNTRY_CODE;
+//                } else {
+//                    TOP_HEADLINES_URL = TOP_HEADLINES_URL.substring(0,TOP_HEADLINES_URL.length()-2) + COUNTRY_CODE;
+//                }
+//                Log.i(TAG, "onNothingSelected: CALLED : " + TOP_HEADLINES_URL);
+//                initiateLoader();
+            }
+        });
+
+        CustomSpinnerAdapter spinnerAdapter = new CustomSpinnerAdapter(getApplicationContext(), flags, countryNames);
+        spinner.setAdapter(spinnerAdapter);
+
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-        mViewPager.setOnTouchListener(new View.OnTouchListener() {
+        rootViewPager.setOffscreenPageLimit(0);
+        rootViewPager.setAdapter(mSectionsPagerAdapter);
+
+        // Disabling the scroll feature
+        rootViewPager.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 return true;
             }
         });
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        rootViewPager.setOffscreenPageLimit(0);
+        rootViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(rootViewPager));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Log.d(TAG, "onTabSelected: " + tab.getText());
+                if (tab.getText().equals(categories[0])) {
+                    getSupportFragmentManager().beginTransaction().replace(fragmentView.getId(),
+                            PlaceholderFragment.newInstance(0, spinner.getSelectedItemPosition())).commit();
+                }
+                if (tab.getText().equals(categories[7])) {
+                    getSupportFragmentManager().beginTransaction().replace(fragmentView.getId(),
+                            PlaceholderFragment.newInstance(7, spinner.getSelectedItemPosition())).commit();
+                }
+            }
 
-        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
 
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
     public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        private static String CATEGORY_URL = "https://newsapi.org/v2/top-headlines?apiKey=d98a0731ab834d3cb605d8ac24dd7072";
 
         public PlaceholderFragment() {
         }
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
+        public static PlaceholderFragment newInstance(int position, int spinnerItemPosition) {
             PlaceholderFragment fragment = new PlaceholderFragment();
             Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            args.putInt(POSITION, position);
+            args.putInt(TAB_POSITION, spinnerItemPosition);
             fragment.setArguments(args);
             return fragment;
         }
@@ -83,17 +176,30 @@ public class CategoryActivity extends AppCompatActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_category, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            final View rootView = inflater.inflate(R.layout.fragment_category, container, false);
+            fragmentView = rootView;
+            int position = getArguments().getInt(POSITION);
+            final String message = "onCreateView: CALLED : COUNTRY = " + countryCodes[getSpinnerPosition()] +
+                    "-" + getSpinnerPosition() + " : CATEGORY = " + categories[getTabPosition()] +
+                    "-" + getTabPosition();
+            String QUERY_URL = BASE_URL + "&country=" + countryCodes[getSpinnerPosition()] +
+                    "&category=" + categories[getTabPosition()].toLowerCase();
+            Log.d(TAG, QUERY_URL);
+            ((TextView) rootView.findViewById(R.id.textview)).setText(message);
+
             return rootView;
         }
+
     }
 
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
+    private static int getTabPosition() {
+        return tabLayout.getSelectedTabPosition();
+    }
+
+    private static int getSpinnerPosition() {
+        return spinner.getSelectedItemPosition();
+    }
+
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
         public SectionsPagerAdapter(FragmentManager fm) {
@@ -102,15 +208,73 @@ public class CategoryActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            PlaceholderFragment fragment = new PlaceholderFragment();//newInstance(position, spinner.getSelectedItemPosition());
+            Bundle args = new Bundle();
+            args.putInt(POSITION, position);
+            args.putInt(TAB_POSITION, getTabPosition());
+            fragment.setArguments(args);
+            return fragment;
         }
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 3;
+            return 8;
         }
     }
+
+//    public static String generateURL() {
+//        String url;
+//        url = BASE_URL + "&country=" + countryCodes[spinner.getSelectedItemPosition()] +
+//                "&category=" + categories[tabLayout.getSelectedTabPosition()];
+//        Log.i(TAG, "generateURL: URL GENERATED : " + url);
+//        return url;
+//    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+// Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_categories) {
+            Intent intent = new Intent(getApplicationContext(), CategoryActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void findViews() {
+        spinner = (Spinner) findViewById(R.id.spinner);
+        loadingIndicator = (ProgressBar) findViewById(R.id.loading_indicator);
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        rootViewPager = (ViewPager) findViewById(R.id.container);
+    }
+
+    private void initFonts() {
+        ROBOTO_LIGHT = Typeface.createFromAsset(getApplicationContext().getAssets(), "fonts/roboto_light.ttf");
+        ROBOTO_THIN = Typeface.createFromAsset(getApplicationContext().getAssets(), "fonts/roboto_thin.ttf");
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
 }
