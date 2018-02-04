d05fdd1fc737e6fae2e225537bcd0864d6f8249f
shantanu0323 <srs.shaan@gmail.com>
2018-02-04 12:59:08 +0530

    feat: Search Layout designed

--- app/src/main/java/com/shaan/newshere/SearchActivity.java
+++ app/src/main/java/com/shaan/newshere/SearchActivity.java
@@ -25,16 +25,8 @@ public class SearchActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
